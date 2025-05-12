ac83170e28480bcd825f57574306699d519aeb60
Helios360 <elias.treguier@gmail.com>
2025-05-12 15:14:27 +0200

    truc

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Routes, Route} from "react-router"
-import Home from "./pages/Home.jsx"
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import {Routes, Route} from "react-router";
+import Home from "./pages/Home.jsx";
+import Layout from "./components/Layout";
 
+function App() {
   return (
-    <>
-	  <Routes>
-	  <Route path="/" element = {<Home />} />
-	  </Routes>
-    </>
+	<Routes>
+		<Route path="/" element={<Layout />}>
+	  		<Route index element={<Home />} />
+		</Route>
+	</Routes>
   )
 }
 
