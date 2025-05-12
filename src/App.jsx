083cbc201a4cabcad5e57820a263b80e16e32683
Helios360 <elias.treguier@gmail.com>
2025-05-12 11:41:50 +0200

    truc

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Routes, Route} from "react-router"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -29,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-	  <Route path="pages/Home.jsx" element = {<Home />}
+	  <Route path="pages/Home.jsx" element = {<Home />} />
 	  </Routes>
     </>
   )
