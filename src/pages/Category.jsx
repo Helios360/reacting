c4e21c4f81454bef37c0799ebb6f82cff7ddd22e
Helios360 <elias.treguier@gmail.com>
2025-05-12 15:51:53 +0200

    truc

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,17 +1,7 @@
-import { Routes, Route } from 'react-router';
-import Home from '../pages/Home';
-import Layout from '../components/Layout';
-import Category from '../pages/Category';
+import { useParams } from 'react-router';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="category/:categoryId" element={<Category />} />
-      </Route>
-    </Routes>
-  );
+export default function Category() {
+  const { categoryId } = useParams();
+
+  return <h1>{categoryId}</h1>;
 }
-
-export default App;
